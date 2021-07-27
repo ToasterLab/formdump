a5e065a299ce57882dc68138cdbf394254cf3dfd
Huey <hello@huey.xyz>
2021-07-27 11:35:20 +0800

    poc

--- project.clj
+++ project.clj
@@ -5,10 +5,14 @@
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [http-kit "2.5.3"]
                  [metosin/jsonista "0.3.3"]
-                 [compojure "1.6.2"]]
+                 [compojure "1.6.2"]
+                 [tick "0.4.32"]
+                 [ring/ring-core "1.9.4"]
+                 [ring/ring-json "0.5.1"]]
   :plugins [[lein-ring "0.12.5"]]
   :ring {:handler formdump.core/app
-         :nrepl {:start true}}
+         :nrepl {:start true
+                 :port "8888"}}
   :main ^:skip-aot formdump.app
   :target-path "target/%s"
   :profiles {:uberjar {:aot :alll}
--- src/formdump/core.clj
+++ src/formdump/core.clj
@@ -1,13 +1,66 @@
 (ns formdump.core
-  (:require [compojure.core :refer [defroutes GET]]
+  (:require [compojure.core :refer [defroutes GET POST]]
             [compojure.route :as route]
+            [compojure.handler :as handler]
+            [ring.util.response :as response]
+            [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
             [org.httpkit.server :refer [run-server]]
-            [jsonista.core :as j])
+            [org.httpkit.client :as http]
+            [org.httpkit.sni-client :as sni-client]
+            [jsonista.core :as json]
+            [tick.alpha.api :as t])
   (:gen-class))
 
-(defroutes app
-  (GET "/" [] "Hello World")
+(alter-var-root #'org.httpkit.client/*default-client* (fn [_] sni-client/default-client))
+
+(defn to-json [data]
+  (json/write-value-as-string data))
+
+(defn iso-date-string []
+  (let [date (t/format :iso-instant (t/zoned-date-time))]
+    (str (subs date 0 23) (subs date 26 27))))
+
+(defn build-answers [field-id data]
+  (to-json [{:questionId field-id
+             :answer1 (to-json data)}]))
+
+(defn build-ms-form-payload [field-id data]
+  (let [date (iso-date-string)
+        answers (build-answers field-id data)]
+    {:startDate date
+     :submitDate date
+     :answers answers}))
+
+(defn ms-form-proxy [form-url field-id data]
+  (let [payload (build-ms-form-payload field-id data)
+        {:keys [status body error]} @(http/post form-url {:body (to-json payload)                                                  :headers {"Content-Type" "application/json"}})]
+    (if error
+      (do
+        (println "Failed" error body)
+        false)
+      (do
+        (println status body)
+        true))))
+
+(defn handle-form-submission [req]
+  (let [{:keys [body]} req
+        url (get-in body ["url"])
+        field-id (get-in body ["field_id"])
+        data (get-in body ["data"])]
+    (if (or (nil? url) (nil? field-id) (nil? data))
+      (do
+        (println url field-id data)
+        "error")
+      (do
+        (ms-form-proxy url field-id data)
+        "success"))))
+
+(defroutes app-routes
+  (GET "/" [] "200")
+  (POST "/" [] handle-form-submission)
   (route/not-found "404 Not Found"))
 
-(defn -main []
-  (run-server app {:port 8000}))
+(def app
+  (-> (handler/api app-routes)
+      (wrap-json-body)
+      (wrap-json-response)))
